edb451ffb6346ef487ef9008ca987c1f10bd2963
lucas rojas <lucasrojas1066@gmail.com>
2023-01-12 23:41:45 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Web server básico que sirve archivos estáticos
+
+## Screenshots
+![Imagen de ejemplo](./screenshots/Screenshot-1.png)
+
+![Imagen de ejemplo](./screenshots/Screenshot-2.png)
---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const hbs = require('hbs');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT;
+
+//Handlebars 
+app.set('view engine', 'hbs'); //renderisar vistas sencillas 
+hbs.registerPartials(__dirname + '/views/partials');
+
+
+//servir contenido estatico
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/generic', (req, res) => {
+  res.render( 'generic' );
+})
+
+app.get('/elements', (req, res) => {
+  res.render( 'elements' );
+})
+
+
+app.get('*', (req, res) => {
+  res.sendFile( __dirname + '/public/template/404.html');
+})
+
+
+app.listen(port, () => {
+  console.log('Example app listening on port ' + port);
+})
